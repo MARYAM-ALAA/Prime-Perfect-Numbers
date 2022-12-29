ed69b628f06b0cd230ae46530ea5c64dbd65cc38
MARYAM-ALAA <116362194+MARYAM-ALAA@users.noreply.github.com>
2022-12-29 23:27:19 +0200

    Add files via upload

---
+++ Perfect Numbers/test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                int n, i, sum, First_Number, Last_Number;
+          
+                Console.WriteLine("Type Two Numbers To Get The Perfect Numbers in Range");
+                Console.Write("------------------------------------------------------");
+         
+                Console.Write("The First Number in Range : ");
+                First_Number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The Last Number in Range : ");
+                Last_Number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("The Perfect Numbers in Range Are : ");
+                for (n = First_Number; n <= Last_Number; n++)
+                {
+                    i = 1;
+                    sum = 0;
+                    while (i < n)
+                    {
+                        if (n % i == 0)
+                            sum = sum + i;
+                        i++;
+                    }
+                    if (sum == n)
+                        Console.Write("{0} ", n);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+    }
+                }
---
+++ Prime Numbers/test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Type Two Numbers To Get The Prime Numbers in Range");
+                Console.WriteLine("________________________________________________");
+
+                Console.WriteLine("Please Enter The First Number Of The Range");
+                int First_Number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please Enter The Last Number Of The Range");
+                int Last_Number = int.Parse(Console.ReadLine());
+               
+                {
+                    for (int i = First_Number; i <= Last_Number; i++)
+                    {
+                        int count = 0;
+                        for (int j = 2; j <= i / 2; j++)
+                        {
+                            if (i % j == 0)
+                            {
+                                count++;
+                                break;
+                            }
+                        }
+                        if (count == 0 && i != 1)
+                            Console.WriteLine(i + " ");
+                    }
+                }
+
+
+
+
+
+            }
+        }
+
+
+
+    }
+
+
+}
+
